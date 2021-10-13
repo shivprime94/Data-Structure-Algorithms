2451a87a005063395a7fc3e05ad2afba5491d1ec
Aanchal1012 <56544911+Aanchal1012@users.noreply.github.com>
2021-10-13 22:21:20 +0530

    added cpp solution of Leetcode problem 70-minor change

--- leetcodeproblems-easy/ClimbingStairs.cpp
+++ leetcodeproblems-easy/ClimbingStairs.cpp
@@ -1,5 +1,5 @@
 /*
-Leetcode - 70 Climbing Tress
+Leetcode - 70 Climbing Stairs
 You are climbing a staircase. It takes n steps to reach the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 */
