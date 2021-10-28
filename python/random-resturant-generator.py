e8fc943976ca1f32ab7fa38433f86c8d04ee86cc
TheHanyu <91917207+TheHanyu@users.noreply.github.com>
2021-10-28 13:33:00 -0700

    Create random-resturant-generator.py

---
+++ python/random-resturant-generator.py
@@ -0,0 +1,10 @@
+import random
+print("Are you always having trouble on deciding where to go eat?")
+print("let me help you!")
+num=random.randint(1, 3)
+if num=1
+  print("go to chiptle!')
+  else if num=2
+        print("go to McDonald's")
+    else
+        print("go to MOD Pizza!")
