3b373e4cf99cac09560f8b9f0f2efb4d0bf304ab
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-10-18 10:47:08 +0530

    Stack related codes in CPP

--- Algorithms CPP/Stack/reverseStack.cpp
+++ Algorithms CPP/Stack/reverseStack.cpp
@@ -1,33 +1,31 @@
-#include<iostream>
-#include<stack>
-using namespace std;
 
-void insertAtBottom(stack<int> &s, int element) {  
-    // base class
-    if(s.empty()) {   
+void insertAtBottom(stack<int> &s, int element) {
+    //basecase
+    if(s.empty()) {
         s.push(element);
-        return;
+      	return ;
     }
     
     int num = s.top();
     s.pop();
-
-    // recursive call
-    insertAtBottom(s, element);   
-    s.push(num);     // push
     
+    //recursive call
+    insertAtBottom(s, element);
+    
+    s.push(num);
 }
 
 void reverseStack(stack<int> &stack) {
-    // base class
-    if(stack.empty()) {   
-        return;
+  	//base case
+    if(stack.empty()) {
+        return ;
     }
-
-    int num = stack.top();    
-    stack.pop();   
-    // recursive call 
+    
+    int num = stack.top();
+    stack.pop();
+    
+    //recursive call
     reverseStack(stack);
-
-    insertAtBottom(stack, num);
+    
+    insertAtBottom(stack,num);
 }
