ac0d172520725d6be3bb863ea2964002cb9e7b8b
Diksha <dikshaprajapati42@gmail.com>
2022-10-23 19:23:31 +0530

    reverse linked list

---
+++ Leetcode Easy/reverse_linked_list.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *nextNode, *prevNode = NULL;
+        while (head) {
+            nextNode = head->next;
+            head->next = prevNode;
+            prevNode = head;
+            head = nextNode;
+        }
+        return prevNode;
+    }
+};
