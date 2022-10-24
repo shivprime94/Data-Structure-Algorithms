140cff51885e2c75da0a64febc2ebcb4558e50cd
Ankit Sharma <78586739+the-iter8@users.noreply.github.com>
2022-10-24 22:19:30 +0530

    Find the Index of the First Occurrence in a String
    Given two strings needle and haystack, return the index of the first occurrence of needle in a haystack, or -1 if the needle is not part of the haystack.
    Topic: Strings.

---
+++ Leetcode Problems/Find_the_Index_of_the_First_Occurrence_in_a_String.py
@@ -0,0 +1,21 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle in haystack:
+            return haystack.index(needle)
+        else:
+            return -1
+            
+## Question - https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
+
+
+''' Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1. '''
